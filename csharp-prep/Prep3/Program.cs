697cafb9b25552d3b9a3e29543e8d4caa914c13a
dbrockbn <dnebooks@gmail.com>
2023-09-16 23:37:14 -0600

    finish homework 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Data.Common;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is the magic number?");
+        // string magic = Console.ReadLine();
+        // int number = int.Parse(magic);
+        // Console.WriteLine("What is your guess?");
+         string guess = Console.ReadLine();
+        int userGuess = int.Parse(guess);
+
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1,101);
+        int number = -1;
+        if (number > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+        else if (number < userGuess)
+            {
+                Console.WriteLine("Lower"); ;
+            }
+        else 
+            {
+                Console.WriteLine("You Guessed it!");                    
+            }
     }
 }
