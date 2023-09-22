a3aa384028072887e52a304ea3ed26b00d5cd09f
dbrockbn <dnebooks@gmail.com>
2023-09-21 20:47:35 -0600

    created job cls resume cls, adding code to program

---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,28 @@
+using System;
+
+ public class Job
+{
+    public string _jobTitle;
+    public string _company;
+
+    public int _startYear;
+
+    public int _endYear;
+
+    public void Display(
+        {
+            Console.WriteLine($"{_company}");
+        }
+    )
+
+
+}
+            
+
+
+// // Job job1 = new Job();
+//     job1._jobTitle = "Software Engineer";
+//     job1._company = "Microsoft";
+//     Job job2 = new Job();
+//     job2._jobTitle = "Manager";
+//     job2._company = "Apple";
